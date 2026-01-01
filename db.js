4c672ca94dc6e19909a1d26aaaf15fe5a971d383
Aryamansood09 <57245083+Aryamansood09@users.noreply.github.com>
2026-01-01 22:57:07 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Qwerty@12345",
-  database: "jersey_store"
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect();
 module.exports = db;
